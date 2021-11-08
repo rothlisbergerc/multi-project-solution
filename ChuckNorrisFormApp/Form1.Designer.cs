d836b77df9f92cab0a5f6e47171cfe94f9d671f2
Cheryl Rothlisberger <RothlisbergerCheryl@gmail.com>
2021-11-08 11:42:24 -0800

    Got everything working except word wrap

--- ChuckNorrisFormApp/Form1.Designer.cs
+++ ChuckNorrisFormApp/Form1.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.jokeText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(224, 243);
+            this.button1.Location = new System.Drawing.Point(53, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(350, 65);
             this.button1.TabIndex = 0;
@@ -46,34 +46,35 @@
             // 
             // comboBox1
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(263, 33);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.Text = "Categories";
+            this.categoryComboBox.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(12, 12);
+            this.categoryComboBox.Name = "comboBox1";
+            this.categoryComboBox.Size = new System.Drawing.Size(263, 33);
+            this.categoryComboBox.TabIndex = 1;
+            this.categoryComboBox.Text = "Categories";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(370, 96);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.jokeText.AutoSize = true;
+            this.jokeText.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.jokeText.Location = new System.Drawing.Point(53, 106);
+            this.jokeText.Name = "label1";
+            this.jokeText.Size = new System.Drawing.Size(63, 25);
+            this.jokeText.TabIndex = 2;
+            this.jokeText.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 349);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(468, 349);
+            this.Controls.Add(this.jokeText);
+            this.Controls.Add(this.categoryComboBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,7 +83,7 @@
         #endregion
 
         private Button button1;
-        private ComboBox comboBox1;
-        private Label label1;
+        private ComboBox categoryComboBox;
+        private Label jokeText;
     }
 }
--- ChuckNorrisFormApp/Form1.cs
+++ ChuckNorrisFormApp/Form1.cs
@@ -1,3 +1,6 @@
+
+using ChuckNorrisAPI;
+
 namespace ChuckNorrisFormApp
 {
     public partial class Form1 : Form
@@ -5,11 +8,27 @@ namespace ChuckNorrisFormApp
         public Form1()
         {
             InitializeComponent();
+        }       
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            // label is blank until the button is clicked.
+            jokeText.Text = "";
+
+            // add categories to category combo box
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var cat in categories)
+            {
+                categoryComboBox.Items.Add(cat);
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Put random joke generator here.";
+            //jokeText.Text = "Put random joke generator here.";
+
+            Joke j = await ChuckNorrisClient.GetRandomJoke();
+            jokeText.Text = j.JokeText;
         }
     }
 }
